346166a21c8b465ccabb64548510096a51186e3c
webader <alexey.derenko@gmail.com>
2023-06-02 08:13:58 +0300

    lesson 1 theory

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
 //MODULES//
-const {sayHello} = require('./helpers/sayHello.helper')
-
-sayHello();
+//імпорт модуля з файла sayHello.helper.js посилання на функцію sayHello
+// const {sayHello} = require('./helpers/sayHello.helper')
+//
+// //викликаємо функцію яку імпортували
+// sayHello();
 
 //GLOBAL VARIABLES//
-__dirname, __filename, process.cwd()
-console.log(__dirname);
-console.log(__filename);
-console.log(process.cwd()); //CWD - current working directory
+// //__dirname, __filename, process.cwd()
+// console.log(__dirname); //виводить шлях до файла з якого викликають
+// console.log(__filename);//виводить шлях включно з файлом який викликає
+// console.log(process.cwd()); //CWD - current working directory
 
 //PATH//
-//модуль допомагає працювати зі шляхами (в він одні показують шлях \ в лінуксі інші/)
-const path = require('path');
-
-const joinPath = path.join(__dirname,'folder', 'folder2','text.txt');
-const normalizePath = path.normalize('///test///test2/////test3//test4');
-const resolvePath = path.resolve('folder', 'folder2','text.txt');
-
-console.log(joinPath);
-console.log(normalizePath);
-console.log(resolvePath);
+//модуль допомагає працювати зі шляхами (в вінді одні показують шлях \ в лінуксі інші/)
+// const path = require('path');
+//
+// const joinedPath = path.join(__dirname,'folder', 'folder2','text.txt');
+// const normalizePath = path.normalize('///test///test2/////test3//test4');
+// const resolvedPath = path.resolve('folder', 'folder2','text.txt');//теж саме що і join тільки не треба прописувати __dirname
+//
+// console.log(joinedPath);
+// console.log(normalizePath);
+// console.log(resolvedPath);
 
 //OS//
-const os = require('os');
-console.log(os.cpus());
-console.log(os.arch());
+const os = require('os'); //підключення модуля OS
+console.log(os.cpus());// покаже інфу про ядра
+console.log(os.arch());//архітектура компа
 
-const{exec}=require('child_process'); // запуск в баш лінуксових скриптів
+const {exec} =require('child_process'); // маємо можливість запускати в баш лінуксових скриптів
 
-//fs//
+// //fs//
 const fs = require('fs');
 const path = require('path');
+
+//Створення файла text2.txt
 fs.writeFile(path.join(__dirname, 'folder', 'folder2', 'text2.txt'), 'Hello from okten again', (err) => {
     if (err) throw new Error(err.message);
-});// створить файл text2.txt
+});
+
+
 // fs.readFile(path.join(__dirname, 'folder', 'folder2', 'text2.txt'), (err, data)=>{
 //  if (err) throw new Error(err.message);
 //  console.log(data);
--- helpers/sayHello.helper.js
+++ helpers/sayHello.helper.js
@@ -2,6 +2,7 @@ function sayHello() {
     console.log('Hello to DEC-2022');
 }
 
+//експорт в ноді
 module.exports = {
     sayHello: sayHello, //можливо скоротити до sayHello
 }
